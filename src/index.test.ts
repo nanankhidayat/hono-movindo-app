05baac1b2b538d85eea6084dea15e8b7d29b108c
xandbox <nanankhidayat@gmail.com>
2025-12-23 10:07:51 +0700

    setup docker-cpmpose

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "bun:test"
+import app from '.'
+
+
+describe('My first test', () => {
+    it('should return 200 response', async () => {
+        const req = new Request('http://localhost/')
+        const res = await app.fetch(req)
+        
+        expect(res.status).toBe(200)
+    })
+})
